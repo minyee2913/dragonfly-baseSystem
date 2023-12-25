c66ec325c1ebe8203c060d0bdac59a1d9de341e7
minyee2913 <leegm0759@gmail.com>
2023-12-25 22:33:40 +0900

    version command

--- system/command/version.go
+++ system/command/version.go
@@ -3,13 +3,36 @@ package command
 import (
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 
 	"github.com/df-mc/dragonfly/server/cmd"
 	"github.com/df-mc/dragonfly/server/player"
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"golang.org/x/mod/modfile"
 )
 
 type VersionCommand struct {
+}
+
+var ignores = []string{
+	"github.com/go-gl/mathgl",
+	"github.com/google/uuid",
+	"github.com/pelletier/go-toml",
+	"github.com/sirupsen/logrus",
+	"github.com/brentp/intintmap",
+	"github.com/cespare/xxhash",
+	"github.com/cespare/xxhash/v2",
+	"github.com/df-mc/atomic",
+	"github.com/go-jose/go-jose/v3",
+	"github.com/golang/protobuf",
+	"github.com/golang/snappy",
+	"github.com/klauspost/compress",
+	"github.com/muhammadmuzzammil1998/jsonc",
+	"github.com/rogpeppe/go-internal",
+	"github.com/segmentio/fasthash",
+	"github.com/df-mc/goleveldb",
+	"github.com/sandertv/go-raknet",
 }
 
 func (c VersionCommand) Run(source cmd.Source, output *cmd.Output) {
@@ -23,11 +46,22 @@ func (c VersionCommand) Run(source cmd.Source, output *cmd.Output) {
 		panic(err)
 	}
 
-	txt := "§bGolang §fv" + f.Go.Version + "\n"
+	txt := "§bGolang §fv" + f.Go.Version + "\n§6Minecraft §fv" + protocol.CurrentVersion + "\n\n"
 
+	i := 0
 	for _, require := range f.Require {
-		if require.Mod.Path == "github.com/df-mc/dragonfly" {
-			txt += "§adf-mc/dragonfly §f" + require.Mod.Version + "\n"
+		if slices.Contains(ignores, require.Mod.Path) {
+			continue
+		}
+
+		if strings.HasPrefix(require.Mod.Path, "github.com/") {
+			txt += "§a" + strings.Replace(require.Mod.Path, "github.com/", "", 1) + "§f " + require.Mod.Version + "\n"
+			i++
+		}
+
+		if i > 12 {
+			txt += "...\n"
+			break
 		}
 	}
 
